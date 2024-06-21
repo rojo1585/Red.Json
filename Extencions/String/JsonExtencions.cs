4e5fe21af5428a452b9cd503ce26ac5664ee305e
Jorge Miguel Mendoza Correa <jorge.mendoza@bodesa.mx>
2024-06-21 11:12:52 -0600

    Renamed Class

--- Extencions/String/JsonExtencions.cs
+++ Extencions/String/JsonExtencions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Red.Json.Extencions.String
+﻿namespace Red.Json.Extensions.String
 {
     internal class JsonExtencions
     {
