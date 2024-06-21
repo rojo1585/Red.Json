4c9c1b6eb5aa767c41945057434822472481d938
Jorge Miguel Mendoza Correa <jorge.mendoza@bodesa.mx>
2024-06-21 11:08:56 -0600

    Add Json helpers

---
+++ Helpers/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace Red.Json.Helpers;
+
+public static class JsonHelper
+{
+    public static string CastToJson(object? json)
+    {
+        string str;
+        using MemoryStream stream = new();
+
+        JsonSerializer.Serialize(stream, json);
+        stream.Seek(0, SeekOrigin.Begin);
+
+        using (StreamReader sr = new(stream))
+        {
+            str = sr.ReadToEnd();
+        }
+        return str;
+    }
+}
